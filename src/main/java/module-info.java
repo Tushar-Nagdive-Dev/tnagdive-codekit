d46f378cfc2deda1699c348d56819fde915c25a0
Tushar Nagdive <codewithtushar840@gmail.com>
2026-06-18 18:02:33 +0530

    Impl: BiChain in progress

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module io.github.tusharnagdive.codekit {
-    requires io.github.tusharnagdive.codekit;
     exports io.github.tusharnagdive.codekit.di;
     exports io.github.tusharnagdive.codekit.kitcollection.unichain;
     exports io.github.tusharnagdive.codekit.kitcollection.bichain;
